879b1f1d71b9b80bb58c3cd6be6071755e4ba292
Ya-Foo <huynhgiaphu3004@gmail.com>
2023-10-23 01:50:17 +0700

    finished team

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("web-page").style.display = "block";
 }
+
+window.onload = function () {
+    var elems = document.getElementsByClassName('card');
+    for (var i in elems) {
+        if (elems[i].nodeType == 1) elems[i].addEventListener('click', function (event) {
+            var mailto_link = 'mailto:' + this.id;
+            var win = window.open(mailto_link, 'emailWindow');
+        });
+    }
+};
